407bd6805a57573f7bce46b07579e4b8dd6398f0
FiyeroHan <tmddn_00@naver.com>
2023-07-11 23:50:18 +0900

    [ADD:DataBase] Coupon 데이터베이스 생성

---
+++ src/database/__coupon__.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from sqlalchemy import Column, String, inspect, Integer
+
+from .__conn__ import *
+
+class CouponTable(Base): # 쿠폰 테이블
+    __tablename__ = 'coupon' #테이블 이름
+    restaurant_name = Column(String(50), nullable=False) # 식당 이름
+    user_id = Column(String(50), primary_key=True, nullable=False) # 유저 아이디
+    coupon_num = Column(Integer(10), nullable=False,) # 쿠폰도장 개수
+
+class Coupon(BaseModel): # 쿠폰
+    restaurant_name: str
+    user_id: int
+    coupon_num: str
+
+
+# CouponTable 테이블 생성
+# 이미 생성되어있는지 확인 후 생성
+# <class 'sqlalchemy.engine.base.Connection'>
+
+
+inspector = inspect(conn.engineData())
+if not inspector.has_table('user'):
+    CouponTable.__table__.create(bind=conn.engineData())
+    print("테이블 생성 완료")
+else:
+    print("이미 테이블이 존재합니다.")
